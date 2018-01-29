3e054c92a6c85430512f17392bca338b00e21633
Dylan <dquesada186@gmail.com>
2018-01-29 08:50:53 -0600

    Added initial class shells.

---
+++ HumaneSociety/SQLConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class SQLConnector
+    {
+        // Member variables
+
+        // Constructor
+
+        // Member methods
+
+    }
+}
