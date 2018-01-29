ef94d6abbc89b0b3ec303d85e344d986ba0b6362
Dylan <dquesada186@gmail.com>
2018-01-29 08:59:54 -0600

    Added db connection string and SQL connection string through SQLConn. constructor.

--- HumaneSociety/SQLConnector.cs
+++ HumaneSociety/SQLConnector.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HumaneSociety
 {
     class SQLConnector
     {
         // Member variables
-
+        public string ConnectionString;
+        SqlConnection con;
         // Constructor
-
+        public SQLConnector()
+        {
+            ConnectionString = "Server = DESKTOP-02SQATS; Database = HumaneSociety; Integrated Security = true;";
+            con = new SqlConnection(ConnectionString);
+        }
         // Member methods
 
     }
