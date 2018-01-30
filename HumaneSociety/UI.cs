b9f670ddbd926351f8f6ca8f2f0f53291e8f0734
Dylan <dquesada186@gmail.com>
2018-01-30 10:29:04 -0600

    Added methods to new UI class to gather string and int data.

---
+++ HumaneSociety/UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class UI
+    {
+        public static int GetIntInput()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            return result;      
+        }
+        public static string GetStringInput()
+        {
+            string value;
+            value = Console.ReadLine().ToString();
+            return value;
+        }
+    }
+}
