102697a2951cac766f084b1d9c3ac685a0ce73c8
Dylan <dquesada186@gmail.com>
2018-01-30 09:35:36 -0600

    Added switch and run method for HS.

--- HumaneSociety/HumaneSociety.cs
+++ HumaneSociety/HumaneSociety.cs
@@ -17,6 +17,38 @@ namespace HumaneSociety
         }
 
         // Member methods
+        public void RunHumaneSociety()
+        {
+            string option;
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("What would you like to do? 'Find' an animal, set an animal for 'adoption', collect a 'payment', 'vaccinate' an animal, ");
+                option = Console.ReadLine().ToLower();
+                switch (option)
+                {
+                    case "find":
+                        break;
+                    case "adoption":
+                        break;
+                    case "payment":
+                        break;
+                    case "vaccinate":
+                        break;
+                    case "done":
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, {0} is not a valid entry. Try Again.", option);
+                        break;
+                }
+            }
+        }
+        public Animal AddAnimal()
+        {
+            Animal animal = new Animal();
 
+            return animal;
+        }
     }
 }
--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -10,6 +10,16 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
+            Animal dog = new Animal();
+            dog.PetName = "dog";
+            
+                using (var db = new GameDbContext())
+                {
+                    var playerQuery = from p in db.Players orderby p.GameId select new { p.Name, p.GameId };
+                    Console.WriteLine("List of Players in DB:");
+                    foreach (var p in playerQuery) Console.WriteLine(p);
+                
+            }
         }
     }
 }
