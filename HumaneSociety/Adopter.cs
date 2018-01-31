4d07157afb6157d30a3244a2246c48cabbd67813
Dylan <dquesada186@gmail.com>
2018-01-31 08:24:45 -0600

    Added class to collect FirstnAme in adopter class.

---
+++ HumaneSociety/Adopter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class Adopter
+    {
+        // Member variables
+        
+        // Constructor
+        public Adopter()
+        {
+
+        }
+        // member methods
+        public string GetFirstName()
+        {
+            Console.WriteLine("Enter your first name: ");
+            return UI.GetStringInput();
+        }
+    }
+}
